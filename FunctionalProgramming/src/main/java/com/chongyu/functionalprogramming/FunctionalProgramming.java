b7fa4675ce27e4bf5e505771d4ff4224d38b873f
Wang Chongyu <chongyu.wang@softgridinc.com>
2018-10-08 01:12:51 +0800

    add code for Function interface

--- FunctionalProgramming/src/main/java/com/chongyu/functionalprogramming/FunctionalProgramming.java
+++ FunctionalProgramming/src/main/java/com/chongyu/functionalprogramming/FunctionalProgramming.java
@@ -1,7 +1,21 @@
 package com.chongyu.functionalprogramming;
 
+import org.junit.Test;
+
+import java.util.function.Function;
+
 public class FunctionalProgramming {
 
+    @Test
+    public void myFPTest() {
+        System.out.println(myFunction.apply(3));
+        System.out.println(myMethod(10, int1 -> int1 * 3));
+    }
 
+    public int myMethod(int pararm1, Function<Integer, Integer> callback) {
+        return pararm1 + callback.apply(3);
+    }
+
+    Function<Integer, Integer> myFunction = int1 -> int1 + 1;
 
 }
